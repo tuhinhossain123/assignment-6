31de63ae8b3ad31b329bb14a082020b3f5d4575d
tuhin <csetuhin55@gmail.com>
2023-09-01 08:37:20 +0600

    nav section added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('file added')
+const loadCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await res.json();
+
+    const tabContainer = document.getElementById('tab-container');
+    data.data.forEach((category) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <button class="bg-gray-200 px-5 py-2 rounded font-semibold">${category.category}</button>
+        `
+        tabContainer.appendChild(div);
+    })
+    console.log(data.data);
+}
+loadCategory();
